48c1d6a92dd752f3626455cdca6d3279c5c9933b
Euael Eshete <euaelmeko@gmail.com>
2022-12-17 18:08:12 +0300

    first draft

---
+++ script.js
@@ -0,0 +1,62 @@
+// create an array of emoji
+const emoji = ['🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🐻', '🐼', '🐨', '🐯', '🦁', '🐮', '🐷', '🐸', '🐵', '🐔', '🐧', '🐦', '🐤', '🐣', '🐥', '🦆', '🦅', '🦉', '🦇', '🐺', '🐗', '🐴', '🦄', '🐝', '🐛', '🦋', '🐌', '🐞', '🐜', '🕷', '🕸', '🐢', '🐍', '🦎', '🦂', '🦀', '🦑', '🐙', '🦐', '🐠', '🐟', '🐡', '🐬', '🦈', '🐳', '🐋', '🐊', '🐅', '🐆', '🦓', '🦍', '🐘', '🦏', '🦛', '🐪', '🐫', '🦒', '🦘', '🐃', '🐂', '🐄', '🐎', '🐖', '🐏', '🐑', '🐐', '🦌', '🐕', '🐩', '🐈', '🐓', '🦃', '🕊', '🐇', '🐁', '🐀', '🐿', '🐾', '🐉', '🐲', '🌵', '🎄', '🌲', '🌳', '🌴', '🌱', '🌿', '☘', '🍀', '🎍', '🎋', '🍃', '🍂', '🍁', '🍄', '🌾', '💐', '🌷', '🌹', '🥀', '🌺', '🌸', '🌼', '🌻', '🌞', '🌝', '🌛', '🌜', '🌚', '🌕', '🌖', '🌗', '🌘', '🌑', '🌒', '🌓', '🌔', '🌙', '🌎', '🌍', '🌏', '🌋', '🌌', '⭐', '🌠', '☄', '☀', '⛅', '☁', '⚡', '☔', '❄', '⛄', '🌀', '🌁', '🌈', '🌊', '🎃', '🎄', '🎆', '🎇', '✨', '🎈', '🎉', '🎊', '🎋', '🎍', '💝', '🎎', '🎏', '🎐', '🎀', '🎁', '🎗', '🎟', '🎫', '🎖', '🏆', '🏅', '🥇', '🥈', '🥉', '⚽', '⚾', '🏀', '🏐', '🏈', '🏉', '🎾', '🎱', '🏓', '🏸', '🏒', '🏑', '🏏', '🏹', '🥊', '🥋', '⛳', '⛸', '🎣', '🎽', '🎿', '🛷', '🥌', '🎯', '🎱', '🎮', '🎰', '🎲', '🧩', '♟', '🎭', '🎨', '🎬', '🎤']
+// length: 202
+
+function setHighScore(score) {
+    let highscore = localStorage.getItem('falling_objects_highscore') || 0
+    if (score >= highscore) {
+        localStorage.setItem('falling_objects_highscore', score)
+    }
+
+    document.getElementById('high_score').innerHTML = localStorage.getItem('falling_objects_highscore')
+}
+
+let score = 0
+setHighScore(score)
+
+function onCollision(e) {
+    var audio = new Audio('./collision.wav');
+    audio.play();
+
+    score -= Math.floor(emoji.indexOf(e.target.innerHTML) / 100) + 1
+    document.getElementById('score').innerHTML = score
+
+    e.target.remove()
+}
+
+function onClick(e) {
+    var audio = new Audio('./click.wav');
+    audio.play();
+
+    score += Math.floor(emoji.indexOf(e.target.innerHTML) / 20) + 1
+    document.getElementById('score').innerHTML = score
+    setHighScore(score)
+    e.target.remove()
+}
+
+// create a function to generate a random emoji
+function createElement() {
+    let newElem = document.createElement('div')
+    newElem.classList.add('obj')
+
+    newElem.style.left = Math.random() * 100 + 'vw'
+    newElem.style.animationDuration = Math.random() * 3 + 10 + 's'
+
+    newElem.innerHTML = emoji[Math.floor(Math.random() * emoji.length)]
+
+    // newElem.innerHTML = '🐹';
+
+    document.body.appendChild(newElem)
+    
+    newElem.addEventListener('click', onClick)
+    newElem.addEventListener('animationend', onCollision)
+
+    setTimeout(() => {
+        newElem.remove()
+    }, 15000)
+
+}
+
+setInterval(() => {
+    createElement()
+}, 300)
